b803c50a05a53b6a04c02d49235888ec11a69001
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-06-02 08:46:00 +0200

    Added files for config

---
+++ src/main/java/com/edu/smartstudentcard/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.edu.smartstudentcard.security;
+
+import com.example.classc.model.User;
+import com.example.classc.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private IUserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String usernameOrMobileOrEmail) throws UsernameNotFoundException {
+		User user = userRepository
+				.findByEmailOrMobileOrUsername(usernameOrMobileOrEmail,usernameOrMobileOrEmail,usernameOrMobileOrEmail)
+				.orElseThrow(() -> new UsernameNotFoundException(
+						"User not found with mobile or email : " + usernameOrMobileOrEmail));
+
+		return UserPrincipal.create(user);
+	}
+
+	@Transactional
+	public UserDetails loadUserById(Long id) {
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found with id : " + id));
+
+		return UserPrincipal.create(user);
+	}
+}
