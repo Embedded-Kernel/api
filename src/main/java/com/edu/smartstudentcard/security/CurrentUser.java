a084a797be7e30d3c2c7b52a0c5b89cfc9b58d52
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-06-02 08:44:36 +0200

    class for CurrentUser

---
+++ src/main/java/com/edu/smartstudentcard/security/CurrentUser.java
@@ -0,0 +1,13 @@
+package com.edu.smartstudentcard.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ ElementType.PARAMETER, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
