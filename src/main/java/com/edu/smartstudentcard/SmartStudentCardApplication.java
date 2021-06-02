b7d9cd6f06443bce0af0748e9f2bd55034624842
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-06-02 08:38:51 +0200

    Initial Project Structure

---
+++ src/main/java/com/edu/smartstudentcard/SmartStudentCardApplication.java
@@ -0,0 +1,13 @@
+package com.edu.smartstudentcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStudentCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartStudentCardApplication.class, args);
+    }
+
+}
